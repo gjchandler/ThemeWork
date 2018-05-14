c9c753a9c1fafe1e2863f44378988ef3eda460f0
haa-gg <glen.gengel@hoglundanalytics.com>
2018-05-13 19:44:49 -0500

    testing

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,4 @@
-//Add read more button to video section  123
+//Add read more button to video section  12345
 console.log('hello world!');
 jQuery('.vid-feed').append(
 "<div class=\"read-more\"> <a href=\"/videos\">read more</a></div>"
